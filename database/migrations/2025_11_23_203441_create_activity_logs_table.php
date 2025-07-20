e3fa94dc3afe152a8519de6f68c398c01691ec2c
katznicho <katznicho@gmail.com>
2025-07-20 22:58:31 +0300

    wip:fixed migration issues

--- database/migrations/2025_11_23_203441_create_activity_logs_table.php
+++ database/migrations/2025_11_23_203441_create_activity_logs_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        
         Schema::create('activity_logs', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique()->index();
