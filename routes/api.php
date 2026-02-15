1054eb1f6cff5f48f93db0a4a5cb9ad94d1b7025
katznicho <katznicho@gmail.com>
2026-02-15 16:43:55 +0300

    Fix namespace case mismatch in InvoiceController
    - Change namespace from App\Http\Controllers\Api to App\Http\Controllers\API
    - Update route references to use uppercase API namespace
    - Matches directory structure and other controllers in API folder

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::prefix('v1')->group(function () {
     include_once __DIR__ . '/custom/mtn_routes.php';
 
     // Invoice API routes for third-party vendors
-    Route::get('/invoices/insurance-company/{insuranceCompanyId}', [\App\Http\Controllers\Api\InvoiceController::class, 'getInvoicesForInsuranceCompany']);
-    Route::post('/invoices/{invoiceId}/mark-paid', [\App\Http\Controllers\Api\InvoiceController::class, 'markInvoiceAsPaid']);
-    Route::get('/invoices/{invoiceId}/details', [\App\Http\Controllers\Api\InvoiceController::class, 'getInvoiceDetails']);
+    Route::get('/invoices/insurance-company/{insuranceCompanyId}', [\App\Http\Controllers\API\InvoiceController::class, 'getInvoicesForInsuranceCompany']);
+    Route::post('/invoices/{invoiceId}/mark-paid', [\App\Http\Controllers\API\InvoiceController::class, 'markInvoiceAsPaid']);
+    Route::get('/invoices/{invoiceId}/details', [\App\Http\Controllers\API\InvoiceController::class, 'getInvoiceDetails']);
 });
