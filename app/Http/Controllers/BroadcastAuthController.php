39ba662ad260b76dbce9b4efd9124ad97afdde68
draletilouis <dralouis01@gmail.com>
2026-03-30 11:38:29 +0300

    Directly sign Reverb auth for known channels

--- app/Http/Controllers/BroadcastAuthController.php
+++ app/Http/Controllers/BroadcastAuthController.php
@@ -6,12 +6,80 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Log;
+use Pusher\Pusher;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Throwable;
 
 class BroadcastAuthController extends Controller
 {
+    protected function makePusher(): Pusher
+    {
+        $config = config('broadcasting.connections.reverb');
+
+        return new Pusher(
+            $config['key'],
+            $config['secret'],
+            $config['app_id'],
+            $config['options'] ?? []
+        );
+    }
+
+    protected function jsonStringResponse(string $payload): SymfonyResponse
+    {
+        return response($payload, 200, ['Content-Type' => 'application/json']);
+    }
+
+    protected function authorizeKnownChannel(Request $request): ?SymfonyResponse
+    {
+        $channelName = (string) $request->input('channel_name', '');
+        $socketId = (string) $request->input('socket_id', '');
+        $user = $request->user();
+
+        if (! $user || $channelName === '' || $socketId === '') {
+            return null;
+        }
+
+        $pusher = $this->makePusher();
+
+        if (preg_match('/^private-user\.(\d+)$/', $channelName, $matches)) {
+            if ((int) $user->id !== (int) $matches[1]) {
+                throw new AccessDeniedHttpException;
+            }
+
+            return $this->jsonStringResponse(
+                $pusher->authorizeChannel($channelName, $socketId)
+            );
+        }
+
+        if (preg_match('/^private-App\.Models\.User\.(\d+)$/', $channelName, $matches)) {
+            if ((int) $user->id !== (int) $matches[1]) {
+                throw new AccessDeniedHttpException;
+            }
+
+            return $this->jsonStringResponse(
+                $pusher->authorizeChannel($channelName, $socketId)
+            );
+        }
+
+        if (preg_match('/^presence-business\.(\d+)$/', $channelName, $matches)) {
+            if ((int) ($user->business_id ?? 0) !== (int) $matches[1]) {
+                throw new AccessDeniedHttpException;
+            }
+
+            return $this->jsonStringResponse(
+                $pusher->authorizePresenceChannel($channelName, $socketId, (string) $user->getAuthIdentifier(), [
+                    'id' => (int) $user->id,
+                    'uuid' => (string) ($user->uuid ?? ''),
+                    'name' => (string) ($user->name ?? ''),
+                    'photo' => (string) ($user->profile_photo_url ?? ''),
+                ])
+            );
+        }
+
+        return null;
+    }
+
     protected function shouldExposeError(Request $request): bool
     {
         return str_starts_with($request->getHost(), 'staging.');
@@ -20,6 +88,10 @@ class BroadcastAuthController extends Controller
     public function __invoke(Request $request): JsonResponse|SymfonyResponse
     {
         try {
+            if ($knownChannelResponse = $this->authorizeKnownChannel($request)) {
+                return $knownChannelResponse;
+            }
+
             $response = Broadcast::auth($request);
 
             if ($response instanceof SymfonyResponse) {
