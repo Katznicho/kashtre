4a70cc1c3c5f818a793b7eea682835c2dea7a1c4
katznicho <katznicho@gmail.com>
2026-04-17 22:28:46 +0300

    end-no-edit

--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -13,6 +13,7 @@
     <link rel="icon" href="{{ asset('images/kashtre_logo.svg') }}" type="image/png" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap" rel="stylesheet" />
 
+    
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @livewireStyles
